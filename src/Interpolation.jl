b72ab788b48501680355ef11192ad12e697a713e
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-09 18:16:47 -0500

    fix typo

--- src/Interpolation.jl
+++ src/Interpolation.jl
@@ -11,7 +11,7 @@ struct Linear{N, T}
     x_max::T
 end
 
-function linear_iterpolation(x::Vector{<:Real}, y::Vector{<:Real})
+function linear_interpolation(x::Vector{<:Real}, y::Vector{<:Real})
     if ! issorted(x)
         error("x must be sorted")
     end
